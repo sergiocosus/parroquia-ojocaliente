d71dd8a161c5352f6bb0cf63639d9d5ed1e1e488
Sergio Contreras Sustaita <sergiocosus@gmail.com>
2019-06-05 13:57:32 -0500

    Fix srcset cycled issue

--- src/app/shared/directives/img-srcset-placeholder.directive.ts
+++ src/app/shared/directives/img-srcset-placeholder.directive.ts
@@ -4,16 +4,21 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: 'img'
 })
 export class ImgSrcsetPlaceholderDirective {
+  loaded = false;
+
   constructor(private el: ElementRef) {
     if (!el.nativeElement.sizes.length) {
       el.nativeElement.sizes = '1px';
     }
   }
 
-  @HostListener('load') onMouseEnter() {
-    this.el.nativeElement.sizes = Math.ceil(
-      this.el.nativeElement.getBoundingClientRect().width /
-      window.innerWidth * 100
-    ) + 'vw';
+  @HostListener('load') load() {
+    if (!this.loaded) {
+      this.loaded = true;
+      this.el.nativeElement.sizes = Math.ceil(
+        this.el.nativeElement.getBoundingClientRect().width /
+        window.innerWidth * 100
+      ) + 'vw';
+    }
   }
 }
