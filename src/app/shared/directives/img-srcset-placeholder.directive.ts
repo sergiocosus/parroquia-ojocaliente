e31ff6dbcaf978a0d454cb300df104c926fb1d3d
Sergio Contreras Sustaita <sergiocosus@gmail.com>
2019-06-05 19:12:40 -0500

    Fix img-srcset issues and gallery and save gallery

--- src/app/shared/directives/img-srcset-placeholder.directive.ts
+++ src/app/shared/directives/img-srcset-placeholder.directive.ts
@@ -1,21 +1,29 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
 
 @Directive({
-  selector: 'img'
+  selector: 'img[srcset]'
 })
 export class ImgSrcsetPlaceholderDirective {
   loaded = false;
+  @HostBinding('sizes') sizes = '1px';
 
-  constructor(private el: ElementRef) {
-    if (!el.nativeElement.sizes.length) {
-      el.nativeElement.sizes = '1px';
+  constructor(private el: ElementRef,
+              @Inject(PLATFORM_ID) private platformId: Object) {
+
+    if (isPlatformServer(this.platformId)) {
+      return;
     }
   }
 
   @HostListener('load') load() {
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
+
     if (!this.loaded) {
       this.loaded = true;
-      this.el.nativeElement.sizes = Math.ceil(
+      this.sizes = Math.ceil(
         this.el.nativeElement.getBoundingClientRect().width /
         window.innerWidth * 100
       ) + 'vw';
