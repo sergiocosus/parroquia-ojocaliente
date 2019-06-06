8ce130f29bf9e2604f14c8898ac63c0599dead9d
Sergio Contreras Sustaita <sergiocosus@gmail.com>
2019-06-06 14:26:12 -0500

    Improve gallery implementation

---
+++ src/app/pages/gallery-page/components/galleries-page/galleries-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GalleryService } from '@app/api/services/gallery.service';
+import { Gallery } from '@app/api/models/gallery.model';
+import { Pagination } from '@app/api/models/pagination';
+import { RouteConstants } from '@app/api/classes/route-constants';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { Lightbox } from '@ngx-gallery/lightbox';
+import { AppMetaService } from '@app/shared/services/app-meta.service';
+import { TranslateService } from '@ngx-translate/core';
+import { GalleryLightboxComponent } from '@app/gallery/components/gallery-lightbox/gallery-lightbox.component';
+
+@Component({
+  selector: 'app-galleries-page',
+  templateUrl: './galleries-page.component.html',
+  styleUrls: ['./galleries-page.component.scss']
+})
+export class GalleriesPageComponent implements OnInit {
+  @ViewChild(GalleryLightboxComponent) galleryLightbox: GalleryLightboxComponent;
+  galleryPagination: Pagination<Gallery>;
+  readonly newGalleryRoute = `/${RouteConstants.admin}/${RouteConstants.gallery}/${RouteConstants.new}`;
+
+  constructor(private galleryService: GalleryService,
+              private route: ActivatedRoute,
+              private lightbox: Lightbox,
+              private router: Router,
+              private metaService: AppMetaService,
+              private translate: TranslateService) {
+  }
+
+  ngOnInit() {
+    this.metaService.update(this.translate.instant('gallery.galleries'));
+
+    this.galleryService.get().pipe(tap(galleryPagination => {
+      this.galleryPagination = galleryPagination;
+    })).subscribe();
+  }
+
+}
