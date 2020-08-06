0d12eac5e70c6f455e4cc84a030586b9356955fc
Sergio Contreras Sustaita <sergiocosus@gmail.com>
2020-08-06 15:51:29 -0500

    Fix guard for server

--- src/app/modules/auth/services/auth-guard.service.ts
+++ src/app/modules/auth/services/auth-guard.service.ts
@@ -1,8 +1,9 @@
 import { map, take } from 'rxjs/operators';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SessionService } from '@app/api/services/session.service';
+import { isPlatformServer } from '@angular/common';
 
 
 /**
@@ -15,10 +16,14 @@ export class AuthGuardService implements CanActivate {
   isLogged = false;
 
   constructor(private sessionService: SessionService,
-              private router: Router) {
+              private router: Router,
+              @Inject(PLATFORM_ID) private platformId: string) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    if (isPlatformServer(this.platformId)) {
+      return route.data['notLogged'];
+    }
     return this.sessionService.getLoggedUser().pipe(map(
       user => {
         const notLogged = route.data['notLogged'];
