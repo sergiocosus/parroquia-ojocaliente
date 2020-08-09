8d3baf618f81e4e74f6b2f3454823b78ac25de74
Sergio Contreras Sustaita <sergiocosus@gmail.com>
2020-08-09 01:32:08 -0500

    Fix gallery for ssr

--- src/app/pages/main-page/components/landing-extra/landing-extra.component.ts
+++ src/app/pages/main-page/components/landing-extra/landing-extra.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { GalleryService } from '@app/api/services/gallery.service';
 import { Gallery } from '@app/api/models/gallery.model';
 import { GalleryItem } from '@ngx-gallery/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-landing-extra',
@@ -13,10 +14,17 @@ export class LandingExtraComponent implements OnInit {
   items: GalleryItem[];
   gallery: Gallery;
 
-  constructor(private galleryService: GalleryService) {
+  constructor(private galleryService: GalleryService,
+              @Inject(PLATFORM_ID) private platformId: Object) {
   }
 
   ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)){
+      this.loadGallery();
+    }
+  }
+
+  private loadGallery() {
     this.galleryService.getOne(this.galleryName).subscribe(gallery => {
       this.gallery = gallery;
       this.items = this.gallery.gallery_pictures.map(picture => ({
@@ -25,5 +33,4 @@ export class LandingExtraComponent implements OnInit {
       }));
     });
   }
-
 }
